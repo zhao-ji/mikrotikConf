3ec689bee810e40ebca9b468e89ba452253c00fe
yulin <byteswarning@gmail.com>
2016-07-25 10:29:09 +0800

    fix: 增加载波机制

--- recieve_DNS_record.py
+++ recieve_DNS_record.py
@@ -7,20 +7,54 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 from scapy.all import sniff
-from scapy.all import DNS, DNSQR, DNSRR
+from scapy.all import send
+from scapy.all import IP, UDP, DNS, DNSQR, DNSRR
+
+CNAME = 5
+
+
+def extract_count_and_order(ip_id, dns_id):
+    return ip_id >> 12 << 16 + dns_id, ip_id % 2 ** 12
+
+
+def get_sport_and_id(count, order):
+    high_bit, low_bit = divmod(count, 2 << 16)
+    return high_bit << 12 + order, low_bit
 
 
 def store(pkg):
-    if pkg.haslayer(DNS) and pkg.haslayer(DNSRR):
+    if pkg.haslayer(DNS) and pkg.haslayer(DNSRR) and pkg[DNS].ancount:
+        # 是DNS 是DNS回答 有CNAME回答内容
+        count, order = extract_count_and_order(pkg[IP].id, pkg[DNS].id)
         for i in range(pkg[DNS].ancount):
-            if pkg[DNSRR][i].type == 1:
-                record = "{domain} {name} {address}\n".format(
+            if pkg[DNSRR][i].type == CNAME:
+                record = "{count} {order} {address} {domain} {name}\n".format(
+                    count=count,
+                    order=order,
+                    address=pkg[DNSRR][i].rdata,
                     domain=pkg[DNSQR].qname.rstrip("."),
                     name=pkg[DNSRR][i].rrname.rstrip("."),
-                    address=pkg[DNSRR][i].rdata,
                 )
                 stdout.write(record)
+                ret_data = pkg[DNSRR][i].rdata
+
+                sport, dns_id = get_sport_and_id(count, order+1)
+
+                dns_query = IP(
+                    dst="8.8.8.8",
+                )/UDP(
+                    sport=sport,
+                    dport=53,
+                )/DNS(
+                    id=dns_id,
+                    rd=1,
+                    qd=DNSQR(
+                        qname=ret_data.strip().rstrip("."),
+                        qtype=CNAME,
+                    ),
+                )
+                send([dns_query, dns_query, dns_query], verbose=0)
 
 if __name__ == "__main__":
-    filter_string = "src host 8.8.8.8 and udp port 53 and udp port 10002"
+    filter_string = "src host 8.8.8.8 and udp src port 53"
     sniff(store=0, filter=filter_string, prn=store)
--- send_DNS_request.py
+++ send_DNS_request.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 """
-查询前一百万域名的CNAME 
+查询前一百万域名的CNAME记录
 """
 
 from sys import stdin
@@ -13,10 +13,31 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import send
 from scapy.all import IP, UDP, DNS, DNSQR
 
-count = 0
+CNAME = 5
 
-for line in stdin:
-    dns_query = IP(dst="8.8.8.8")/UDP(sport=10002, dport=53)/DNS(
-        rd=1, qd=DNSQR(qname=line.strip(), qtype=5),
-    )
-    send([dns_query, dns_query, dns_query], verbose=0)
+
+def get_sport_and_id(seed):
+    high_bit, low_bit = divmod(seed, 2**16)
+    return high_bit << 12, low_bit
+
+
+count = 1
+
+if __name__ == "__main__":
+    for line in stdin:
+        sport, dns_id = get_sport_and_id(seed=count)
+        dns_query = IP(
+            dst="8.8.8.8",
+        )/UDP(
+            sport=sport,
+            dport=53,
+        )/DNS(
+            id=dns_id,
+            rd=1,
+            qd=DNSQR(
+                qname=line.strip(),
+                qtype=CNAME,
+            ),
+        )
+        send([dns_query, dns_query, dns_query], verbose=0)
+        count += 1
